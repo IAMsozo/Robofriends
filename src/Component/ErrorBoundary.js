39888c194ea2c030d710b056dd5bdb19b8ce822b
Tolu <Tolu@users.noreply.github.com>
2019-12-07 16:33:50 +0100

    little change

--- src/Component/ErrorBoundary.js
+++ src/Component/ErrorBoundary.js
@@ -4,7 +4,7 @@ class ErrorBoundary extends React.Component{
     constructor (props){
         super(props);
         this.state = {
-            hasError:false;
+            hasError:false
         }
     }
 
